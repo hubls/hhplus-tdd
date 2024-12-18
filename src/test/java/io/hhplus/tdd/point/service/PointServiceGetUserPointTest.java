230f9493ddef1faf97026f13dad7b29aa9919eaf
leesangjun <hubls@naver.com>
2024-12-18 14:56:58 +0900

    🚨 Test: 유저 포인트 내역 조회 기능 테스트

--- src/test/java/io/hhplus/tdd/point/service/PointServiceGetUserPointTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceGetUserPointTest.java
@@ -1,5 +1,7 @@
 package io.hhplus.tdd.point.service;
 
+import io.hhplus.tdd.point.PointHistory;
+import io.hhplus.tdd.point.TransactionType;
 import io.hhplus.tdd.point.UserPoint;
 import io.hhplus.tdd.point.repository.PointHistoryRepository;
 import io.hhplus.tdd.point.repository.UserPointRepository;
@@ -8,6 +10,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.eq;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,5 +46,46 @@ public class PointServiceGetUserPointTest {
 
         verify(userPointRepository).findById(eq(userId));
         verify(userPointRepository, times(1)).findById(eq(userId));
+    }
+
+    @Test
+    void 포인트내역_조회_성공케이스() {
+        // given
+        long cursor = 1;
+        long userId = 123L;
+        long chargingPoint = 5000L;
+        long usePoint = 1000L;
+        List<PointHistory> pointHistories = new ArrayList<>();
+        PointHistory chargingPointHistory = new PointHistory(cursor++, userId, chargingPoint, TransactionType.CHARGE, System.currentTimeMillis());
+        PointHistory usingPointHistory = new PointHistory(cursor, userId, usePoint, TransactionType.USE, System.currentTimeMillis());
+        pointHistories.add(chargingPointHistory);
+        pointHistories.add(usingPointHistory);
+
+        // when
+        when(pointHistoryRepository.findAllById(eq(userId))).thenReturn(pointHistories);
+        List<PointHistory> result = pointService.getUserPointHistories(userId);
+
+        // then (포인트내역 제대로 반환 되었는지 검증)
+        assertThat(result.get(0)).isEqualTo(chargingPointHistory);
+        assertThat(result.get(1)).isEqualTo(usingPointHistory);
+
+        // verify 추가: pointHistoryRepository.findAllById가 userId를 가지고 한 번 호출되었는지 확인
+        verify(pointHistoryRepository).findAllById(eq(userId));
+        verify(pointHistoryRepository, times(1)).findAllById(eq(userId));
+    }
+
+    @Test
+    void 포인트내역_조회_실패케이스_사용내역존재하지않음() {
+        long userId = 123L;
+
+        // given
+        when(pointHistoryRepository.findAllById(eq(userId))).thenReturn(List.of()); // 비어 있는 리스트 반환
+
+        // when
+        List<PointHistory> result = pointService.getUserPointHistories(userId);
+
+        // then
+        assertThat(result).isEmpty(); // 결과가 비어 있어야 함
+        verify(pointHistoryRepository, times(1)).findAllById(eq(userId)); // 호출 검증
     }
 }
